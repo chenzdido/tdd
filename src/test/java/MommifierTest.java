0d750dd81c991f3298fa89e0c7849cd4250c7687
chenzdido <chenzdido@gmail.com>
2020-08-03 21:39:51 +0800

    [zhe] feat:refactoring delete extra code.

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -6,7 +6,6 @@ public class MommifierTest {
     void should_insert_mommy_when_convert_given_string_is_aaple(){
         Mommifier mommifier=new Mommifier();
         String result=mommifier.convert("aaple");
-        System.out.println(result);
         assertEquals("amommyaple",result);
     }
 
