3cbbd1db3af04313ff0e56f5083d1cec1f0c103f
chenzdido <chenzdido@gmail.com>
2020-08-03 19:36:58 +0800

    [zhe] feat:third test given string without continuous of vowel

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -9,10 +9,19 @@ public class MommifierTest {
         System.out.println(result);
         assertEquals("amommyaple",result);
     }
+
     @Test
     void should_return_original_string_when_convert_given_string_vowel_length_less_30(){
         Mommifier mommifier=new Mommifier();
         String result=mommifier.convert("dog");
         assertEquals("dog",result);
     }
+
+    @Test
+    void should_return_original_string_when_convert_given_string_without_continuous_of_vowel(){
+        Mommifier mommifier=new Mommifier();
+        String result=mommifier.convert("opela");
+        assertEquals("opela",result);
+    }
+
 }
