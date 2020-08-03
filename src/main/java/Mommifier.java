f7564cfa1701697edfd69704287c6d49fb8fc7b0
chenzdido <chenzdido@gmail.com>
2020-08-03 19:33:46 +0800

    [zhe] feat:second test if the numberof volwels less 30% return origin string

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,7 +1,33 @@
 public class Mommifier {
 
 
-    public String convert() {
-        return "amommyaple";
+    public String convert(String str) {
+        StringBuilder s=new StringBuilder(str);
+        int length=s.length();
+        int vowel_numbers=0;
+        int[] n=new int[length];
+        int j=0;
+        for(int i=0 ; i<length ; i++){
+            char ch = s.charAt(i);
+            if( ch == 'a'|| ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                vowel_numbers ++;
+                if((i<length-1)&&(s.charAt(i+1) == 'a'|| s.charAt(i+1) == 'e' || s.charAt(i+1) == 'i' || s.charAt(i+1) == 'o' || s.charAt(i+1) == 'u')){
+                    n[j]=i+1;
+                    j++;
+                }
+            }
+        }
+        double percent=(double)vowel_numbers/(double)length;
+        if (percent< 0.3||j==0) {
+            return s.toString();
+        }else{
+            for(int k=0 ;k<j;k++){
+                s.insert(n[k]+k*5,"mommy");
+                }
+            return s.toString();
+            }
+
     }
+
 }
+
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -3,9 +3,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MommifierTest {
     @Test
-    void shoule_return_mommy_when_convert__given_string_is_aaple(){
+    void should_insert_mommy_when_convert_given_string_is_aaple(){
         Mommifier mommifier=new Mommifier();
-        String result=mommifier.convert();
+        String result=mommifier.convert("aaple");
+        System.out.println(result);
         assertEquals("amommyaple",result);
+    }
+    @Test
+    void should_return_original_string_when_convert_given_string_vowel_length_less_30(){
+        Mommifier mommifier=new Mommifier();
+        String result=mommifier.convert("dog");
+        assertEquals("dog",result);
     }
 }
